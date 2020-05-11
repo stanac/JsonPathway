12b351349d58aa84c82e6956ea12b3f740c3ab7d
Milan Stanacev <stanacevm@gmail.com>
2020-05-11 16:15:37 +0200

    license, readme and tests for readme

--- readme.md
+++ readme.md
@@ -15,6 +15,7 @@ that depends only on [System.Text.Json](https://www.nuget.org/packages/System.Te
 | `*` | Wildcard for properties |
 | `[*]` | Wildcard for array elements (useless?) |
 | `[?()]` | Filter for object properties or array elements |
+| `@` | Current element refernce in filter |
 
 `()` script expression is not supported in this implementation
 
@@ -105,7 +106,21 @@ For all examples following JSON will be used as input (taken from
 
 For path `$.store.bicycle.color.length` method `ExecutePath` returns JSON array `[3]`;
 
+For path `$.store.book[?(@.title.length == 21)]` resulting JSON array is:
+```json
+[
+  { "category": "fiction",
+    "author": "J. R. R. Tolkien",
+    "title": "The Lord of the Rings",
+    "isbn": "0-395-19395-8",
+    "price": 22.99
+  }
+]
+```
+
 ## Supported methods
+
+**Methods are supported only in filters**
 
 #### Supported string methods
 
@@ -123,5 +138,147 @@ For path `$.store.bicycle.color.length` method `ExecutePath` returns JSON array 
 #### Supported array methods
  - `contains(element: any)`
 
-### Supported methods example
+##### Supported methods example
 
+Path `$.store.book[?(@.author.contains("tolkien", true))]` returns
+
+```json
+[
+  {
+    "category": "fiction",
+    "author": "J. R. R. Tolkien",
+    "title": "The Lord of the Rings",
+    "isbn": "0-395-19395-8",
+    "price": 22.99
+  }
+]
+```
+
+Same goes for path `$.store.book[?(@.author.contains('tolkien', true))]` even though single 
+quotes are not supported by "specification" they are supported by this implementation for string quotes.
+
+##### Other examples
+
+Following child operators all return same result (`[19.95]`):
+ - `$.store.bicycle.price`
+ - `store.bicycle.price`
+ - `$["store"]["bicycle"]["price"]`
+ - `["store"]["bicycle"]["price"]`
+ - `$['store']['bicycle']['price']`
+ - `['store']['bicycle']['price']`
+
+Which means `$` is optional and strings can be quoted with `'` and `"`.
+
+Array operators:
+ - `$.store.book[0]` returns "Sayings of the Century" book
+ - `$.store.book[-1]` returns "The Lord of the Rings" book (last book)
+ - `$.store.book[*]` returns all books
+ 
+Slice operator [start:end:step] `$.store.book[0:4:2]` returns books at indexes [0] and [2]
+(second number "end" is exclusive).
+
+Wildcard can be applied to object properties with `.*`:
+- `$.store.bicycle.*` returns `["red",19.95]`
+
+Recursive operator can be applied to properties and arrays with `..` e.g.
+
+`$.store.book..` results in
+```json
+[
+  [
+    {
+      "category": "reference",
+      "author": "Nigel Rees",
+      "title": "Sayings of the Century",
+      "price": 8.95
+    },
+    {
+      "category": "fiction",
+      "author": "Evelyn Waugh",
+      "title": "Sword of Honour",
+      "price": 12.99
+    },
+    {
+      "category": "fiction",
+      "author": "Herman Melville",
+      "title": "Moby Dick",
+      "isbn": "0-553-21311-3",
+      "price": 8.99
+    },
+    {
+      "category": "fiction",
+     "author": "J. R. R. Tolkien",
+     "title": "The Lord of the Rings",
+     "isbn": "0-395-19395-8",
+     "price": 22.99
+    }
+  ],
+  {
+  	"category": "reference",
+  	"author": "Nigel Rees",
+  	"title": "Sayings of the Century",
+  	"price": 8.95
+  },
+  {
+  	"category": "fiction",
+  	"author": "Evelyn Waugh",
+  	"title": "Sword of Honour",
+  	"price": 12.99
+  },
+  {
+  	"category": "fiction",
+  	"author": "Herman Melville",
+  	"title": "Moby Dick",
+  	"isbn": "0-553-21311-3",
+  	"price": 8.99
+  },
+  {
+    "category": "fiction",
+    "author": "J. R. R. Tolkien",
+    "title": "The Lord of the Rings",
+    "isbn": "0-395-19395-8",
+    "price": 22.99
+  }
+]
+```
+
+Filters can be applied to array elements and object property values:
+ 
+- `$.store.book[?(@.price > 10)]` returns:
+```json
+[
+  {
+    "category": "fiction",
+    "author": "Evelyn Waugh",
+    "title": "Sword of Honour",
+    "price": 12.99
+  },
+  {
+    "category": "fiction",
+    "author": "J. R. R. Tolkien",
+    "title": "The Lord of the Rings",
+    "isbn": "0-395-19395-8",
+    "price": 22.99
+  }
+]
+```
+
+- `$.store.book[?(@.isbn)]` (truthy filter) returns:
+```json
+[
+  {
+    "category": "fiction",
+    "author": "Herman Melville",
+    "title": "Moby Dick",
+    "isbn": "0-553-21311-3",
+    "price": 8.99
+  },
+  {
+    "category": "fiction",
+    "author": "J. R. R. Tolkien",
+    "title": "The Lord of the Rings",
+    "isbn": "0-395-19395-8",
+    "price": 22.99
+  }
+]
+```
--- tests/JsonPathway.Tests/ReadMeExamplesTests.cs
+++ tests/JsonPathway.Tests/ReadMeExamplesTests.cs
@@ -1,4 +1,5 @@
 ﻿using JsonPathway.Internal;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using Xunit;
@@ -7,7 +8,16 @@ namespace JsonPathway.Tests
 {
     public class ReadMeExamplesTests
     {
-        private string _json = TestDataLoader.Store();
+        private string _json;
+
+        private string _lotr;
+
+        public ReadMeExamplesTests()
+        {
+            _json = TestDataLoader.Store();
+
+            _lotr = @"{""category"":""fiction"",""author"":""J. R. R. Tolkien"",""title"":""The Lord of the Rings"",""isbn"":""0-395-19395-8"",""price"":22.99}";
+        }
 
         [Fact]
         public void StringLength_ReturnsCorrectResult()
@@ -15,6 +25,24 @@ namespace JsonPathway.Tests
             string path = "$.store.bicycle.color.length";
             var result = JsonPath.ExecutePath(path, _json);
             Assert.Equal(3, result.Single().GetInt32());
+        }
+
+        [Fact]
+        public void ArrayLength_ReturnsCorrectResult()
+        {
+            string path = "$.store.book.length";
+            var result = JsonPath.ExecutePath(path, _json);
+            Assert.Equal(4, result.Single().GetInt32());
+        }
+
+        [Fact]
+        public void StringLengthInFilter_ReturnsCorrectResult()
+        {
+            string path = "$.store.book[?(@.title.length == 21)]";
+            var result = JsonPath.ExecutePath(path, _json);
+            Assert.Single(result);
+            string resultString = JsonSerializer.Serialize(result.Single());
+            Assert.Equal(_lotr, resultString);
         }
 
         [Fact]
@@ -27,6 +55,84 @@ namespace JsonPathway.Tests
             JsonElement result1 = JsonPath.ExecutePath(expression, doc).Single();
             JsonElement result2 = JsonPath.ExecutePath(expression, doc).Single();
             Assert.Equal(result1, result2, JsonElementEqualityComparer.Default);
+        }
+
+        [Fact]
+        public void StringMethodToUpper_ReturnsCorrectResult()
+        {
+            string path = "$.store.book[?(@.author.contains(\"tolkien\", true))]";
+            IReadOnlyList<JsonElement> result = JsonPath.ExecutePath(path, _json);
+
+            Assert.Single(result);
+            string resultString = JsonSerializer.Serialize(result.Single());
+            Assert.Equal(_lotr, resultString);
+        }
+
+        [Fact]
+        public void PropertyAndEscapedProperty_ReturnsSameResult()
+        {
+            IReadOnlyList<JsonElement> result1 = JsonPath.ExecutePath("$.store.bicycle.price", _json);
+            IReadOnlyList<JsonElement> result2 = JsonPath.ExecutePath("$[\"store\"][\"bicycle\"][\"price\"]", _json);
+            IReadOnlyList<JsonElement> result3 = JsonPath.ExecutePath("$['store']['bicycle']['price']", _json);
+
+            Assert.Equal(result1.Single(), result2.Single(), JsonElementEqualityComparer.Default);
+            Assert.Equal(result2.Single(), result2.Single(), JsonElementEqualityComparer.Default);
+        }
+
+        [Fact]
+        public void SliceOperator_ReturnsCorrectResult()
+        {
+            IReadOnlyList<JsonElement> result = JsonPath.ExecutePath("store.book[0:4:2]", _json);
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public void WildcardOperator_ReturnsCorrectResult()
+        {
+            IReadOnlyList<JsonElement> result = JsonPath.ExecutePath("$.store.bicycle.*", _json);
+            Assert.Equal(2, result.Count);
+            Assert.Contains(result, x => x.ValueKind == JsonValueKind.String && x.GetString() == "red");
+            Assert.Contains(result, x => x.ValueKind == JsonValueKind.Number && x.GetDouble() == 19.95);
+        }
+
+
+        [Fact]
+        public void RecursiveOperator_ReturnsCorrectResult()
+        {
+            IReadOnlyList<JsonElement> result = JsonPath.ExecutePath("$.store.book..", _json);
+            Assert.Equal(5, result.Count);
+
+            string expected = "[[{`category`:`reference`,`author`:`Nigel Rees`,`title`:`Sayings of the Century`,`price`:8.95},{`category`:`fiction`,`author`:`Evelyn Waugh`,`title`:`Sword of Honour`,`price`:12.99},{`category`:`fiction`,`author`:`Herman Melville`,`title`:`Moby Dick`,`isbn`:`0-553-21311-3`,`price`:8.99},{`category`:`fiction`,`author`:`J. R. R. Tolkien`,`title`:`The Lord of the Rings`,`isbn`:`0-395-19395-8`,`price`:22.99}],{`category`:`reference`,`author`:`Nigel Rees`,`title`:`Sayings of the Century`,`price`:8.95},{`category`:`fiction`,`author`:`Evelyn Waugh`,`title`:`Sword of Honour`,`price`:12.99},{`category`:`fiction`,`author`:`Herman Melville`,`title`:`Moby Dick`,`isbn`:`0-553-21311-3`,`price`:8.99},{`category`:`fiction`,`author`:`J. R. R. Tolkien`,`title`:`The Lord of the Rings`,`isbn`:`0-395-19395-8`,`price`:22.99}]"
+                .Replace("`", "\"");
+            string resultJson = JsonSerializer.Serialize(result);
+            Assert.Equal(expected, resultJson);
+        }
+
+        [Fact]
+        public void FitlerOperatorOnPrice_ReturnsCorrectResult()
+        {
+            IReadOnlyList<JsonElement> result = JsonPath.ExecutePath("$.store.book[?(@.price > 10)]", _json);
+            Assert.Equal(2, result.Count);
+
+            string resultJson = JsonSerializer.Serialize(result);
+            string expectedJson = "[{`category`:`fiction`,`author`:`Evelyn Waugh`,`title`:`Sword of Honour`,`price`:12.99},{`category`:`fiction`,`author`:`J. R. R. Tolkien`,`title`:`The Lord of the Rings`,`isbn`:`0-395-19395-8`,`price`:22.99}]"
+                .Replace("`", "\"");
+
+            Assert.Equal(expectedJson, resultJson);
+        }
+
+
+        [Fact]
+        public void FitlerTruthyIsbnOperator_ReturnsCorrectResult()
+        {
+            IReadOnlyList<JsonElement> result = JsonPath.ExecutePath("$.store.book[?(@.isbn)]", _json);
+            Assert.Equal(2, result.Count);
+
+            string resultJson = JsonSerializer.Serialize(result);
+            string expectedJson = "[{`category`:`fiction`,`author`:`Herman Melville`,`title`:`Moby Dick`,`isbn`:`0-553-21311-3`,`price`:8.99},{`category`:`fiction`,`author`:`J. R. R. Tolkien`,`title`:`The Lord of the Rings`,`isbn`:`0-395-19395-8`,`price`:22.99}]"
+                .Replace("`", "\"");
+
+            Assert.Equal(expectedJson, resultJson);
         }
     }
 }
