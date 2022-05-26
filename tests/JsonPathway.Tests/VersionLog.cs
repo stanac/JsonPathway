38bba3276401a4fbc9e7301a1a41f885fc5cc1e7
stanac <stanacevm@gmail.com>
2022-05-26 17:24:20 +0200

    Support multiple versions of System.Text.Json

---
+++ tests/JsonPathway.Tests/VersionLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace JsonPathway.Tests
+{
+    public class VersionLog
+    {
+        private readonly ITestOutputHelper _testOutput;
+
+        public VersionLog(ITestOutputHelper testOutput)
+        {
+            _testOutput = testOutput;
+        }
+
+        [Fact]
+        public void Log()
+        {
+            FileVersionInfo info = FileVersionInfo.GetVersionInfo(typeof(JsonDocument).Assembly.Location);
+            _testOutput.WriteLine($"Testing using: {info.FileVersion}");
+        }
+    }
+}
